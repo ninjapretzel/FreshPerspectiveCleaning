36b61996e82cc06874e73026024159456e5f2af5
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-29 16:37:27 -0600

    Adds some back end notes

--- server/app.js
+++ server/app.js
@@ -3,17 +3,27 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const corsConfig = {
-	origin: 'http://localhost:3000', // This should come from a config file!
+	// FORGOT TO MENTION THIS: THIS IS NECESSARY FOR HAVING A SEPARATE FRONTEND AND BACKEND HOST
+	// By default (thankfully, a secure default), any requests that come to your server,
+	// but _are not from the same hostname_ are blocked. 
+	// In order to unblock them, we must provide configuration.
+	// In this case, we want to allow requests that are from the React front end.
+	// (This is called Cross-Origin-Resource-Sharing, or CORS)
+	// (problems caused by the default CORS rejections are SUPER COMMON when developing,
+	// but, if it was not this way, web applications would be SUPER INSECURE)
+	origin: 'http://localhost:3000', // also, this should come from a config file!
 }
 
+// App creation and configuration:
 const app = express();
 app.use(cors(corsConfig));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+// Attach login router
 const login = require("./routes/login");
 app.use(login);
 
+// Listen on different port than front-end
 const port = 3001
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
