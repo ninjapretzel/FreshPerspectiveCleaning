84a4d50ff62a04dc892229541c00f92e713d1b95
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-29 16:33:03 -0600

    Adds comments

--- client/src/App.js
+++ client/src/App.js
@@ -27,38 +27,64 @@ import "./css/Footer.css"
 import './css/App.css';
 import Global from './Global'
 
+// Class components are more explicit definitions of components than functional components.
+// see https://medium.com/@Zwenza/functional-vs-class-components-in-react-231e3fbd7108#dd07 for more information.
+// apparently, class components can use lifecycle methods now, but they use hooks, but I would still typically stick with class Components.
 class App extends React.Component {
+  // Class-components are typically initialized in their constructor
   constructor() {
-    super();
+    super(); 
+    // this is typically where one would set up their component's state.
+    // Every class-component automatically has a capture like
+    // let [ state, setState ] = useState({});
+    // and then those captured variables are placed into the object:
+    // this.state = state;
+    // this.setState = setState;
+    
+    // In App, this state will become the application's Global context.
+    // We can put any global data/functions here.
+    // Use this pattern sparingly- too much global state is good way to make your app unmaintainable!
     this.state = {
-      username: null,
-      token: null,
-      onLogin: (creds) => {
+      username: null, // Currently logged in user's username
+      token: null, // Currently logged in user's access token
+      onLogin: (creds) => { // used when the user has been logged in.
         localStorage["userLogin"] = JSON.stringify(creds);
         this.setState(creds);
         window.location = "/"
       },
-      logout: () => {
+      logout: () => { // Used when the user should be logged out 
         localStorage["userLogin"] = "";
         this.setState({ username: null, token: null });
         window.location = "/"
       }
     }
   }
-  
+  // Class-components are able to bind more lifecycle methods than just the default `render()`
+  // See more about component lifecycle: https://reactjs.org/docs/react-component.html#the-component-lifecycle
+  // or a diagram here: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
   async componentDidMount() {
-		if (localStorage["userLogin"]) {
-			const userLogin = JSON.parse(localStorage["userLogin"]);
-			const {username, token} = userLogin;
-			
-			// TODO: Verify token is still valid
-			this.setState({username, token});
-			
-		}
-	}
+    // After this component is mounted, we'll check for login information
+    if (localStorage["userLogin"]) {
+      // If we have a login, we'll parse it out, and update our state.
+      const userLogin = JSON.parse(localStorage["userLogin"]);
+      const {username, token} = userLogin;
+      this.setState({username, token});
+      
+      // TODO: Verify token is still valid
+      // I might make a /checkLogin route on the server that would do this, 
+      // and would return an object like `{ valid: true }` or `{ valid: false }`
+      // if the token is not valid, then it would use the `this.state.logout` function.
+    }
+  }
 
   render() {
     return (
+      // A react context (like Global) has a `Provider` component defined within it.
+      // We'll wrap the entire application in a `Global.Provider`, using the App's `this.state` as the value.
+      // This will make the application's state object become globally accessible.
+      // In this case, we are using it to minimize friction when accessing/using login information
+      // But I will reiterate again- do this sparingly, 
+      // and only put things in the global state if you know you will actually need them app-wide.
       <Global.Provider value={this.state}>
         <Router>
           <Navbar />
--- client/src/pages/EmployeePortal.js
+++ client/src/pages/EmployeePortal.js
@@ -2,71 +2,119 @@ import React from "react"
 import Global from "../Global"
 import axios from "axios"
 
+// Modified by ninjapretzel
 // TODO: Grab this from configuration!
 const BACKEND_HOST = "http://localhost:3001";
 
+// Class components are able to do much more than function components are.
+// see https://medium.com/@Zwenza/functional-vs-class-components-in-react-231e3fbd7108#dd07 for more information.
+// # Functional vs Class-Components in React
+// ## (Differences between functional and class-Components)
+
 class EmployeePortal extends React.Component {
+	// One of the other features of react, can be wired in automatically by react.
+	// If we assign a `static contextType` to point to a context type (like Global)
+	// react will automatically look "up the tree" for a matching context Provider
+	// in this case, it will be found inside of the `App`- the `<Global.Provider>` that everything is inside of inside of App.
 	static contextType = Global;
 	
+	// this is typically where one would set up their component's state.
+	// Every class-component automatically has a capture like
+	// let [ state, setState ] = useState({});
+	// and then those captured variables are placed into the object:
+	// this.state = state;
+	// this.setState = setState;
 	constructor() {
 		super();
+		// State is the things we are going to remember.
+		// In this case, it is information entered into the form by the user.
+		// if you were using a functional component, this would be a little uglier:
+		// let [ username, setUsername ] = useState("");
+		// let [ password, setPassword ] = useState("");
+		// let [ message, setMessage ] = useState("");
+		// And this would also complicate the `captureInput` method down below.
 		this.state = {
 			username: "",
 			password: "",
 			message: ""
 		}
 	}
+	// This is a function that we bind to the Input elements of the form, to their `onChange`.
+	// this gets called any time a key is typed within that element.
 	captureInput = (event) => {
-		const varName = event.target.id;
-		const value = event.target.value;
+		// Read the id and value of what was changed
+		const {id, value} = event.target;
 		
-		const payload = {}
-		payload[varName] = value;
+		// Create an object
+		const payload = {};
+		// Set the field named by the 'id' (eg, username for the element like <input id="username">)
+		// to the value in the input field
+		payload[id] = value;
 		
+		// Update the component's state with that new information (which will re-render the component)
 		this.setState(payload);
 	}
 	
+	// Function for logic to talk with the server.
 	doLogin = async () => {
+		// Unpack the username/password in the form
 		const { username, password } = this.state;
-		//console.log(this.context);
 		
+		// Send them to the backend's "/login" route
+		// and wait for the results.
 		const result = await axios.post(BACKEND_HOST + "/login", {
 			username, password
 		})
 		
+		// Unpack the result
 		const { success, token, message } = result.data;
 		
-		console.log(this.context);
 		if (success) {
+			// If we were successful, call the global context's `onLogin` function.
 			this.context.onLogin({ username, token });
 		} else {
+			// If it failed, set the message to show up underneath the login form in red.
 			this.setState({ message });
 		}
 	}
 	
 	render() {
+		// check if we're logged in.
+		// this data comes from the context.
+		// if either is provided, consider ourselves logged in.
 		return (this.context.username || this.context.token) ? (
+			// If logged in, show them their options
+			// I would also suggest moving this stuff out into another, separate component file.
+			// and even potentially making a separate file for every control you may want to give an employee
+			//		eg, make one component for allowing an employee to set their available times, 
+			//		and another to show their next job (if available)
 			<div>
 				<h2>You are logged in as {this.context.username}</h2>
 				<a href="#" onClick={this.context.logout} >Logout!</a>
 			</div>
 		) : (
-			<div className="row">
-			<h1>Employee Portal: employee login</h1>
+			// Otherwise, if they are not logged in, show them a login page.
 			
-			<div className="input-field col s12">
-				<input onChange={this.captureInput} id="username" type="text" className="validate" />
-				<label for="username">Username</label>
-			</div>
-			
-			<div className="input-field col s12">
-				<input onChange={this.captureInput} id="password" type="password" className="validate" />
-				<label for="password">Password</label>
-			</div>
-			
-			<a onClick={this.doLogin} id="loginButton" className="waves-effect waves-light btn">login!</a>
-			
-			<div className="red-text">{this.state.message}</div>
+			<div className="row container"> 
+				<h1>Employee Portal: employee login</h1>
+				
+				{ /* general structure is taken from Materialize's input examples https://materializecss.com/text-inputs.html */ }
+				{/* The wired 'onChange' is reusable for every input. */}
+				<div className="input-field col s12">
+					<input onChange={this.captureInput} id="username" type="text" className="validate" />
+					<label for="username">Username</label>
+				</div>
+				
+				<div className="input-field col s12">
+					<input onChange={this.captureInput} id="password" type="password" className="validate" />
+					<label for="password">Password</label>
+				</div>
+				
+				{/* Button for doing the login routine */}
+				<a onClick={this.doLogin} id="loginButton" className="green darken-2 waves-effect waves-light btn">login</a>
+				
+				{/* Display area for the error message */}
+				<div className="red-text">{this.state.message}</div>
 			</div>
 		)
 	}
