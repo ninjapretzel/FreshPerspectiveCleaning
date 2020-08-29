440bdb707dc5a51694da047366b7e4c9f57d54da
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-29 16:56:37 -0600

    Adds comments about db's index

--- server/db/index.js
+++ server/db/index.js
@@ -9,22 +9,18 @@ mongoose.connect(`mongodb://localhost:27017/expressReactLogin`, {
 const UserSchema = require("./user");
 const User = mongoose.model("User", UserSchema);
 
+// Little function to make admin account the first time the server starts up.
 async function ensureAdminExists() {
+	// See if one exists,
 	const check = await User.findOne( { username: "admin" } )
-	
+	// And if not, create it.
 	if (!check) { // No admin user? Create a default one.
-		const hash = await argon2.hash("changeMe");
-		
-		await User.create({
-			username: "admin",
-			hash,
-			role: "Admin",
-		});
-		
+		// Use argon2 to hash the default password.
+		const hash = await argon2.hash("changeMe");// default admin password should be a hint.
+		// Store the username/hash/role in the database.
+		await User.create({ username: "admin", hash, role: "Admin" });
 	}
-	
-}
-ensureAdminExists(); 
+} ensureAdminExists(); // And fire this function immediately when loading this file.
 
 module.exports = {
 	User,
