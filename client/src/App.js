4b9de1a085af4c2b4050fc0dd65d2f8af61d91a7
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-29 15:19:37 -0600

    Reworks App into a Class component, wraps entire app in a global context

--- client/src/App.js
+++ client/src/App.js
@@ -25,24 +25,58 @@ import 'materialize-css/dist/css/materialize.min.css'
 import "./css/Navbar.css"
 import "./css/Footer.css"
 import './css/App.css';
+import Global from './Global'
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <div className="container app-content">
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route exact path="/checklist" component={ Checklist } />
-          <Route exact path="/estimate" component={ Estimate } />
-          <Route exact path="/booking" component={ Booking } />
-          <Route exact path="/employee" component={ EmployeePortal } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </div>
-      <Footer />
-    </Router>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: null,
+      token: null,
+      onLogin: (creds) => {
+        localStorage["userLogin"] = JSON.stringify(creds);
+        this.setState(creds);
+        window.location = "/"
+      },
+      logout: () => {
+        localStorage["userLogin"] = "";
+        this.setState({ username: null, token: null });
+        window.location = "/"
+      }
+    }
+  }
+  
+  async componentDidMount() {
+		if (localStorage["userLogin"]) {
+			const userLogin = JSON.parse(localStorage["userLogin"]);
+			const {username, token} = userLogin;
+			
+			// TODO: Verify token is still valid
+			this.setState({username, token});
+			
+		}
+	}
+
+  render() {
+    return (
+      <Global.Provider value={this.state}>
+        <Router>
+          <Navbar />
+          <div className="container app-content">
+            <Switch>
+              <Route exact path="/" component={ Home } />
+              <Route exact path="/checklist" component={ Checklist } />
+              <Route exact path="/estimate" component={ Estimate } />
+              <Route exact path="/booking" component={ Booking } />
+              <Route exact path="/employee" component={ EmployeePortal } />
+              <Route path="*" component={ NotFound } />
+            </Switch>
+          </div>
+          <Footer />
+        </Router>
+      </Global.Provider>
+    );
+  }
 }
 
 export default App;
