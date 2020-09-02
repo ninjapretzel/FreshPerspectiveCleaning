7a7e5b97823402c752a044c5650f94930fcc0616
Brandon Gart <brandongart@gmail.com>
2020-09-02 16:54:45 -0600

    adds images to carousel on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,45 @@
 import React from "react"
+import M from "materialize-css"
+import { Carousel } from "react-materialize"
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+import img1 from "../images/1.jpg"
+import img2 from "../images/2.jpg"
+import img3 from "../images/3.jpg"
+import img4 from "../images/4.jpg"
+import img5 from "../images/5.jpg"
+import img6 from "../images/6.jpg"
+import img7 from "../images/7.jpg"
+import img8 from "../images/8.jpg"
+import img9 from "../images/9.jpg"
+import img10 from "../images/10.jpg"
+
+const images = [ 
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+]
+
+class Home extends React.Component {
+	
+	render() {
+		return (
+			<Carousel 
+				carouselId="HomeCarousel"
+				images={images}
+				options={{
+					fullWidth: true,
+				}}
+			
+			/>
+		)
+	}
 }
 
 export default Home
