0a8065a48d1d728ca0d4149ef68246be9f140c78
Brandon Gart <brandongart@gmail.com>
2020-09-04 14:57:45 -0600

    adds login form

--- client/src/pages/EmployeePortal.js
+++ client/src/pages/EmployeePortal.js
@@ -1,13 +1,42 @@
 import React from "react"
 import axios from "axios"
+import { Redirect } from "react-router-dom";
 
-const EmployeePortal = () => {
-
-    return (
-        <div className="container app-content">
-            <h3>Employee Portal: employee login page</h3>
-        </div>
-    )
+class EmployeePortal extends React.Component {
+    state = {
+        username: "",
+        password: "",
+    }
+    
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.history.push("/");
+    }
+    
+    captureInput = (event) => {
+        const payload = {}
+		payload[event.target.id] = event.target.value;
+		this.setState(payload);
+    }
+    
+    render() {
+        // console.log("In Render:", this.state);
+        return (
+            <div className="container app-content row">
+                <h3>Employee Portal: employee login page</h3>
+                <div className="input-field col s12">
+                    <input id="username" onChange={this.captureInput} type="email" />
+                    <label for="username">Username</label>
+                </div>
+                <div className="input-field col s12">
+                    <input id="password" onChange={this.captureInput} type="password" />
+                    <label for="password">Password</label>
+                </div>
+                
+                <a className="btn" href="" onClick={this.onSubmit}>Login</a>
+            </div>
+        )
+    }
 }
 
 export default EmployeePortal
