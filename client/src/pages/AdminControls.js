05b659e57368706b80db9185067aeb3d9565d08e
Brandon Gart <brandongart@gmail.com>
2020-09-04 15:59:27 -0600

    Adds content to differentiate admin and employee controls

--- client/src/pages/AdminControls.js
+++ client/src/pages/AdminControls.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 class AdminControls extends React.Component {
-
+	render() {
+		return <div className ="row container app-content">
+			<h3>Admin Controls</h3>
+		</div>
+	}
 }
 
 export default AdminControls;
--- client/src/pages/EmployeeControls.js
+++ client/src/pages/EmployeeControls.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 class EmployeeControls extends React.Component {
-
+	render() {
+		return <div className ="row container app-content">
+			<h3>Employee Controls</h3>
+		</div>
+	}
 }
 
 export default EmployeeControls;
