79cdd75c0be478de010b2b06e0eb8fa8a34541f4
Brandon Gart <brandongart@gmail.com>
2020-09-04 15:32:05 -0600

    Hooks up login route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
-const apiRoutes = require("./apiRoutes")
 
-router.use("/api", apiRoutes)
+router.use("/api", require("./apiRoutes"))
+router.use("/login", require("./login"))
 
 module.exports = router
