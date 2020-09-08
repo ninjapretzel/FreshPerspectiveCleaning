caa18d78bd1e7c37bee14d60db52795347c36756
Simon Xu <simon@liyaintl.com>
2020-09-08 12:21:01 -0600

    block dates in calendar for full booked date and holidays

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 // import dependencies
+const axios = require("axios")
+
 const Holidays = require('date-holidays');
 
 const hd = new Holidays('US', 'CO');
@@ -55,16 +57,29 @@ const dataFromDB = [
 
 const shouldBlock = [];
 
+axios.get("/api/getjobs")
+            .then(({data}) => {
+                for(let i = 0; i < data.length - 1; i++) {
+                            shouldBlock.push(data[i].selectedDate);
+                }
+                console.log("#############################")
+                console.log(shouldBlock)
+
+            })
+            .catch(err => console.log(err))
+
+// const shouldBlock = [];
+
 // suppose that the date should be blocked if the date has its dupilcate in database. 
 // didn't count on how many duplicates there are. it should be no more than 2 duplicates since there are no more than 2 sections per day
 // for simplicity, we don't consider abnormal case that has more than 2 duplicates.
-for(let i = 0; i < dataFromDB.length - 1; i++) {
-    for(let j = i + 1; j < dataFromDB.length; j++) {
-        if(dataFromDB[j].date.getTime() === dataFromDB[i].date.getTime()) {
-            shouldBlock.push(new Date(dataFromDB[i].date));
-        }
-    }
-}
+// for(let i = 0; i < dataFromDB.length - 1; i++) {
+//     for(let j = i + 1; j < dataFromDB.length; j++) {
+//         if(dataFromDB[j].date.getTime() === dataFromDB[i].date.getTime()) {
+//             shouldBlock.push(new Date(dataFromDB[i].date));
+//         }
+//     }
+// }
 
 const blockDates = shouldBlock.concat(hdDates)
 console.log("QQQQQQQQQQQQQQQQQQQQQQQQQQQ")
@@ -87,3 +102,6 @@ function checkIfdisable(selectedDate) {
         }
     });
 }
+
+
+// MONGODB_URI=mongodb+srv://finalProject:project12345678@cluster0.k29do.mongodb.net/freshperspective?retryWrites=true&w=majority
